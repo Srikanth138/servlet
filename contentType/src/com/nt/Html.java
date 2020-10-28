59617e6d6d93eb52e9488dad1253456b9c9bf2b3
srika <srika@LAPTOP-81P5HPDV>
2020-10-28 10:46:38 +0530

    MIME AND Refresh

---
+++ contentType/src/com/nt/Html.java
@@ -0,0 +1,35 @@
+package com.nt;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Html extends HttpServlet {
+	
+
+	protected void service(HttpServletRequest req,HttpServletResponse res) throws IOException,ServletException{
+		
+		res.setContentType("text/plain");
+		res.setContentType("text/html"); //last type will store res.type
+		
+		
+		PrintWriter pw=res.getWriter();
+		pw.print("<table bgcolor='cyan' border='2' align='center'>");
+		pw.println("<tr><th>Name</th> <th>AGE</th> <th>AREA</th></tr>");
+		pw.println("<tr><td>Srikanth</td> <td>24</td> <td>Hyd</td></tr>");
+		pw.println("<tr><td>vish</td> <td>10</td> <td>Jgl</td></tr>");
+		pw.println("<tr><td>raju</td> <td>20</td> <td>Jpr</td></tr>");
+		pw.println("</table>");
+		
+		//res.setHeader("refresh","5");
+		res.setIntHeader("refresh", 2);
+		pw.println("<h1 style=color:red;align:center>Date is changeing"+new java.util.Date()+"</h1>");
+		pw.close();
+	}
+ 
+
+}
